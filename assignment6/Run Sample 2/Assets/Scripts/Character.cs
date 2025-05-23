d5fbc179a9742b788ab4cd0c37c9e37952def4d0
Twuel <qkrwjdgns1239@gmail.com>
2025-05-23 19:33:32 +0900

    과제5번 제출

--- assignment6/Run Sample 2/Assets/Scripts/Character.cs
+++ assignment6/Run Sample 2/Assets/Scripts/Character.cs
@@ -34,15 +34,17 @@ public class Character : MonoBehaviour
         // tag가 Platform인 것과 충돌하면 RemainJump를 초기화한다.
         // tag가 Obstacle인 것과 충돌하면 게임 오버한다.
         // ---------- TODO ---------- 
-        
+
         // -------------------- 
+        GM.GameOver();
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         // tag가 Point인 것과 충돌하면 Point를 하나 얻고, 충돌한 오브젝트를 삭제한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if(col.CompareTag("Point"))
+        {
+            Destroy(col.gameObject);
+        }
     }
 }
